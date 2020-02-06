960ab6e596bbb94664911514ae624e4417aedcc1
Jhonatan Ramirez <jhramirez@turnkeytec.com>
2020-02-05 19:47:20 -0600

    ADAL TO BC V1

---
+++ BCTestConnectApi/BCAPIReq.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using RestSharp;
+using Newtonsoft.Json;
+using AuthenticationContext = Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net;
+using System.IO;
+
+namespace BCTestConnectApi
+{
+    class BCAPIReq
+    {
+        const string ClientRedirectUrl = "https://localhost";
+        static string clientId = "APPID";
+        static string clientSecret = "SECRET APP KEY";
+        static string accessToken = null;
+        static string resource = "https://api.businesscentral.dynamics.com";
+        static string bcResponse;
+        static string authorityUriv2 = "https://login.microsoftonline.com/e53d2a43-6254-40d3-bf7e-6cb773a25285";
+        
+        static void Main(string[] args)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Ssl3 | System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls;
+            
+            ClientCredential clientCredential = new ClientCredential(clientId, clientSecret);
+            var authenticationContext = new AuthenticationContext(authorityUriv2, true);
+            var authContext = new AuthenticationContext(authorityUriv2);
+            var authenticationResult = authContext.AcquireTokenAsync(resource, clientId, new Uri(ClientRedirectUrl), new PlatformParameters(PromptBehavior.Auto)).GetAwaiter().GetResult();
+            accessToken = authenticationResult.AccessToken;
+
+            //var clientBC = new RestClient("https://api.businesscentral.dynamics.com/v2.0/Production/api/CertTech/Integrations/v1.0");
+            var clientBC = new RestClient("https://api.businesscentral.dynamics.com/v2.0/Production/api/CertTech/Integrations/v1.0/companies");
+            clientBC.Timeout = -1;
+            var requestBC = new RestRequest(Method.GET);
+            requestBC.AddHeader("Authorization", "Bearer " + accessToken);
+            IRestResponse responseBC = clientBC.Execute(requestBC);
+            bcResponse = responseBC.Content;
+            object resposeDataBC = JsonConvert.DeserializeObject(bcResponse);
+
+            Console.Write(resposeDataBC);
+            Console.ReadKey();
+        }
+        public static string HttpGet(string URI)
+        {
+            string html = string.Empty;
+            
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                HttpResponseMessage response = client.GetAsync(new Uri(URI)).Result;
+                response.EnsureSuccessStatusCode();
+                string result = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine("Result: " + result);
+            }
+
+            return html;
+        }
+
+        public static async Task Auth()
+        {
+            using (HttpClient httpclient = new HttpClient())
+            {
+                httpclient.Timeout = new TimeSpan(0, 2, 0);
+                httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                HttpResponseMessage response = await httpclient.GetAsync("https://api.businesscentral.dynamics.com/v2.0/Production/api/CertTech/Integrations/v1.0/companies");
+            }
+        }
+    }
+}
---
+++ BCTestConnectApi/Program.cs
@@ -0,0 +1,101 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+
+namespace BCTestConnectApi
+{
+    class Program
+    {
+        // Azure AD registrations:
+        // Specifies the Azure AD tenant ID
+        const string AadTenantId = "TurnkeyTec.onmicrosoft.com";
+        static string clientId = "d7f6b8e1-b3a0-4454-b4bb-fb8de6a3e2b5";
+        static string clientSecret = "=_X@gsLAvoRNQR?WsvSASrDMjx4c6G82";
+
+        // The server base address
+        //static string baseUrl = "https://login.windows.net/e53d2a43-6254-40d3-bf7e-6cb773a25285/oauth2/authorize?resource=https://api.businesscentral.dynamics.com";
+        static string baseUrl = "https://login.windows.net/e53d2a43-6254-40d3-bf7e-6cb773a25285/oauth2/token?resource=https://api.businesscentral.dynamics.com";
+        //static string baseUrl = "https://login.windows.net/e53d2a43-6254-40d3-bf7e-6cb773a25285/oauth2/";
+
+        // this will hold the Access Token returned from the server.
+        static string accessToken = null;
+
+        //static void Main(string[] args)
+        //{
+        //    try
+        //    {
+        //        //Console.WriteLine("Starting ...");
+        //        //var clientTk = new RestClient(baseUrl);
+        //        //clientTk.Timeout = -1;
+        //        //var requestTk = new RestRequest(Method.POST);
+        //        //IRestResponse responseTk = clientTk.Execute(requestTk);
+
+
+
+        //        DoIt().Wait();
+        //        var client = new RestClient("https://api.businesscentral.dynamics.com/v2.0/Development/api/CertTech/Integrations/v1.0");
+        //        client.Timeout = -1;
+        //        var request = new RestRequest(Method.GET);
+        //        request.AddHeader("Authorization", "Bearer " + accessToken);
+        //        IRestResponse response = client.Execute(request);
+        //        Console.WriteLine(accessToken);
+        //        //Console.ReadLine();
+        //    }
+        //    catch (Exception e)
+        //    {
+
+        //    }
+        //}
+
+        private static async Task<int> DoIt()
+        {
+            // Get the Access Token.
+            accessToken = await GetAccessToken();
+            Console.WriteLine(accessToken != null ? "Got Token" : "No Token found");
+            return 0;
+        }
+        /// <summary>
+        /// This method uses the OAuth Client Credentials Flow to get an Access Token to provide
+        /// Authorization to the APIs.
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<string> GetAccessToken()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+
+                // We want the response to be JSON.
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // Build up the data to POST.
+                List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
+                postData.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+                postData.Add(new KeyValuePair<string, string>("redirect_uri", "https://businesscentral.dynamics.com"));
+                postData.Add(new KeyValuePair<string, string>("client_id", clientId));
+                postData.Add(new KeyValuePair<string, string>("client_secret", clientSecret));
+                postData.Add(new KeyValuePair<string, string>("username", "jhramirez@turnkeytec.com"));
+                postData.Add(new KeyValuePair<string, string>("password", "Welcome!TK"));
+
+                FormUrlEncodedContent content = new FormUrlEncodedContent(postData);
+
+                // Post to the Server and parse the response.
+                HttpResponseMessage response = await client.PostAsync("token", content);
+                string jsonString = await response.Content.ReadAsStringAsync();
+                object responseData = JsonConvert.DeserializeObject(jsonString);
+
+                // return the Access Token.
+                return ((dynamic)responseData).access_token;
+            }
+        }
+    }
+}
